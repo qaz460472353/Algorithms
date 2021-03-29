1b6b2080287babdf03fa84fb348db93769365464
qaz460472353 <zhaoyu5250>
2021-03-29 23:00:48 +0800

    省市县三级联动实现

--- src/main/java/com/Algorithms/中科三清/ReadJsonFile.java
+++ src/main/java/com/Algorithms/中科三清/ReadJsonFile.java
@@ -1,13 +1,48 @@
 package com.Algorithms.中科三清;
 
+import com.alibaba.fastjson.JSON;
+
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReadJsonFile {
 
+    static List<String> names = new ArrayList<>();
+
     public static void main(String[] args) {
-        String json = readJsonFile("src/main/com/Algorithms/中科三清/省市县三级联动.json");
-        System.out.println(json);
+        String json = readJsonFile("src/main/resources/省市县三级联动.json");
+        List<Node> nodes = JSON.parseArray(json, Node.class);
+        List<String> nodes1 = getNodes(nodes, 2,null);
+        System.out.println(nodes1.toString());
     }
+
+    public static List<String> getNodes(List<Node> nodes, int n,String nodeName) {
+        if (nodes == null || n <= 0) {
+            return null;
+        }
+
+        for (Node node : nodes) {
+            if (nodeName == null) {
+                nodeName = node.name;
+            } else {
+               nodeName += ("-"+node.name);
+            }
+
+            if (n == 1) {
+                names.add(nodeName);
+                nodeName = null;
+            }
+            
+            if (node.children.size() > 0 && n > 1) {
+                getNodes(node.children,n-1,nodeName);
+            }
+        }
+        return names;
+    }
+
+
+
     /**
      * 读取json文件，返回json串
      * @param fileName
@@ -32,6 +67,44 @@ public class ReadJsonFile {
         } catch (IOException e) {
             e.printStackTrace();
             return null;
+        }
+    }
+
+    static class Node {
+        String code;
+        String name;
+        List<Node> children;
+
+        public Node() {}
+
+        public Node(String code, String name, List<Node> children) {
+            this.code = code;
+            this.name = name;
+            this.children = children;
+        }
+
+        public String getCode() {
+            return code;
+        }
+
+        public void setCode(String code) {
+            this.code = code;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public List<Node> getChildren() {
+            return children;
+        }
+
+        public void setChildren(List<Node> children) {
+            this.children = children;
         }
     }
 }
